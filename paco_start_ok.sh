70a6dfffcda4646151329bcca671494c562861e3
luca.spano <luca.spano@wellnet.it>
2020-10-21 17:01:49 +0200

    aggiornamento con tag progress per run su AWS

--- paco_start_ok.sh
+++ paco_start_ok.sh
@@ -26,6 +26,9 @@ brand=$(php Start.php -b "${selection}")
 echo running on ${brand}
 cd ..
 	## use this command to run on AWS
-docker run --rm -v "$PWD/behat.yml:/tests/behat.yml" -v "$PWD/features:/tests/features" -v "$PWD/artifacts:/tests/artifacts" -v "$PWD/vendor:/tests/vendor" -v "$PWD/credentials.json:/tests/credentials.json" -v "$PWD/token.json:/tests/token.json" wellnetimages/behat:2.0.1 /tests/bin/behat --format pretty features/features_${brand}/TSF050-utils/TSF050_FT002-order_placer.feature:4
+	## output:
+	## --format pretty for debug)
+	## --format progress no debug)
+docker run --rm -v "$PWD/behat.yml:/tests/behat.yml" -v "$PWD/features:/tests/features" -v "$PWD/artifacts:/tests/artifacts" -v "$PWD/vendor:/tests/vendor" -v "$PWD/credentials.json:/tests/credentials.json" -v "$PWD/token.json:/tests/token.json" wellnetimages/behat:2.0.1 /tests/bin/behat --format progress features/features_${brand}/TSF050-utils/TSF050_FT002-order_placer.feature:4
 	## use this command to run locally
 ## rm -rf artifacts/screenshots/*.png & docker run --rm --network tests_default -v "$PWD/behat.yml:/tests/behat.yml" -v "$PWD/features:/tests/features" -v "$PWD/artifacts:/tests/artifacts" -v "$PWD/vendor:/tests/vendor" -v "$PWD/credentials.json:/tests/credentials.json" -v "$PWD/token.json:/tests/token.json" wellnetimages/behat:2.0.0 /tests/bin/behat --format pretty features/features_${brand}/TSF050-utils/TSF050_FT002-order_placer.feature:4
